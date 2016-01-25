bc5010a52c169d28de232a9ddc052148d2c5500c
adbecker <ab4035@nyu.edu>
2016-01-25 11:26:09 -0500

    typo in tsiRdata...

--- R/tsiRdata_function.R
+++ R/tsiRdata_function.R
@@ -2,8 +2,8 @@ tsiRdata <- function(time,cases,births,pop,IP=2){
   
   intcases <- as.numeric(tapply(cases, (seq_along(cases)-1) %/% IP, sum))
   intweek <- time[seq(1, length(time), IP)]
-  intbirths <- approx(births,n=length(biweek))$y / (52 / IP)
-  intpop <- approx(pop,n=length(biweek))$y 
+  intbirths <- approx(births,n=length(intweek))$y / (52 / IP)
+  intpop <- approx(pop,n=length(intweek))$y 
   
   data <- as.data.frame(cbind(intweek,intcases,intbirths,intpop))
   names(data) <- c('time','cases','births','pop')
