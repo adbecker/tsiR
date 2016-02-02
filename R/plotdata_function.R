debfbfbbdc0e1540c331efd6e990d66c8950b456
adbecker <ab4035@nyu.edu>
2016-02-02 13:44:08 -0500

    adding plotdata function

---
+++ R/plotdata_function.R
@@ -0,0 +1,10 @@
+plotdata <- function(data){  
+  
+  p <- ggplot(melt(data,id='time'),aes(x=time,y=value))+geom_line(colour='dodgerblue',size=1)+
+    facet_wrap(~variable,ncol=1,scales="free_y")+
+    theme_bw()
+  
+  return(p)
+}
+
+
