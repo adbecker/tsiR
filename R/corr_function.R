ced306d73b29443694ea70443611f40963871b9b
adbecker <ab4035@nyu.edu>
2016-02-03 15:15:46 -0500

    starting to get the package ready for CRAN so deleting extra and updating description and namespace

--- R/corr_function.R
+++ R/corr_function.R
@@ -4,7 +4,6 @@
 
 corr <- function(sim){
   
-  
   if(class(sim) == "list"){  
     sim <- sim$res
   }
@@ -15,7 +14,11 @@ corr <- function(sim){
   obs <- sim$cases
   pred <- sim$mean
   fit <- lm(pred ~ obs)
-  c1 <- ggreg(fit)+xlab('observed')+ylab('predicted')
+  rsquared <- signif(summary(fit)$adj.r.squared, 2)
+  pval <- signif(summary(fit)$coef[2,4], 2)
+  c1 <- ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() + stat_smooth(method = "lm", col = "black") +theme_bw()+
+    ggtitle(bquote(Adjusted~' '~R^{2}==.(rsquared)))+xlab('observed')+ylab('predicted')
   print(c1)
 }
 
