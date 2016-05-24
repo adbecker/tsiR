9b80abb5be1c3681baed6fdda2bf6fcd0fc93e59
adbecker <ab4035@nyu.edu>
2016-05-24 15:12:31 -0400

    correlation number should be the log corr not just corr as est on log scale

---
+++ R/logcorr_function.R
@@ -0,0 +1,32 @@
+#' @title logcorr
+#'
+#' @description Plot the correlation of the true data against the fitted resimulated data.
+#'
+#' @param sim The dataframe or list produced by the 'runtsir' function.
+#' @examples
+#' require(tsiR)
+#' require(kernlab)
+#' London <- twentymeas[["London"]]
+#' res <- runtsir(London)
+#' logcorr(res)
+
+logcorr <- function(sim){
+  
+  if(class(sim) == "list"){
+    sim <- sim$res
+  }
+  if(class(sim) == "data.frame"){
+    sim <- sim
+  }
+  
+  obs <- log(sim$cases+1)
+  pred <- log(sim$mean+1)
+  fit <- lm(pred ~ obs)
+  rsquared <- signif(summary(fit)$adj.r.squared, 2)
+  pval <- signif(summary(fit)$coef[2,4], 2)
+  c1 <- ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) +
+    geom_point() + stat_smooth(method = "lm", col = "black") +theme_bw()+
+    ggtitle(bquote(Adjusted~' '~R^{2}==.(rsquared)))+xlab('log(observed)')+ylab('log(predicted)')
+  return(c1)
+}
+
